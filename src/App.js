a56bef91fa8773e4bc7fe58a36b0739e424e8f74
Vivek <viveknikate.2000@gmail.com>
2024-06-23 17:04:21 +0530

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import validator from 'validator';
 
 function App() {
+  const [psswd, setNewPsswd] = useState('');
+
+  const isRight = (value) => {
+    if (value.length === 0) {
+      return <h4 style={{color: "grey"}}>Please enter password to check its Strength.</h4>
+    }
+    if (validator.isStrongPassword(value, {
+      minLength: 8, minLowercase: 1, minUppercase: 1, minSymbols: 1, minNumbers: 1
+    })) {
+      return <h4 style={{color:'green'}}> It's Strong password</h4>
+    } else {
+      return <h4 style={{color:'red'}}>Password is not Strong <span style={{color: 'green'}}>(must contain at least 8 chars, 1 number, 1 symbol, 1 lowercase, 1 Uppercase)</span> </h4>
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1  id='title'>Enter password to check it's Strength: </h1>
+      <input type="text" id="passwordInput" placeholder="enter password" onChange={(e) => setNewPsswd(e.target.value)}/>
+      <h3 id="passwordStrength">{psswd}</h3>
+      <span id="strengthIndicator">
+        {isRight(psswd)}
+      </span>
     </div>
   );
-}
+};
 
 export default App;
